55dae31b934a59bd724eab60b6823aa79e160e1b
sebastian <sroa54@ucatolica.edu.co>
2026-09-18 19:34:32 -0500

    feat(informe): implementar informe de resultados con radar calculado [HU-013]

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Login from './paginas/Login.jsx'
 import Registro from './paginas/Registro.jsx'
 import Panel from './paginas/Panel.jsx'
 import Diagnostico from './paginas/Diagnostico.jsx'
-import Resultados from './paginas/Resultados.jsx'
+import InformeResultados from './paginas/InformeResultados.jsx'
 import Historial from './paginas/Historial.jsx'
 
 export default function App() {
@@ -15,7 +15,7 @@ export default function App() {
       <Route path="/registro" element={<Registro />} />
       <Route path="/panel" element={<Panel />} />
       <Route path="/diagnostico" element={<Diagnostico />} />
-      <Route path="/resultados/:id" element={<Resultados />} />
+      <Route path="/resultados/:id" element={<InformeResultados />} />
       <Route path="/historial" element={<Historial />} />
     </Routes>
   )
